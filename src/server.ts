c270c0d84701c46dae70b39839e9c2ac1ca6a729
Possomato <gabrielb.possomato@gmail.com>
2025-07-01 14:15:38 -0300

    add - post

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,16 @@ import express from 'express'
 const app = express()
 const port = 8080
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('hello world 2')
+})
+
+app.post('/product', (req, res) => {
+  const product = (req.body)
+
+  res.send(product)
 })
 
 app.listen(port)
